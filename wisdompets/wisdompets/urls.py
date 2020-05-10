b4b3589fb964ad3a2e4a6e3b7131cda052a355bb
tnaswin <tnaswin007@gmail.com>
2020-05-10 22:33:22 +0530

    Implementing Django Views

--- wisdompets/wisdompets/urls.py
+++ wisdompets/wisdompets/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from adoptions import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
 ]
