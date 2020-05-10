619dc5ede0021b74010401047aa53e79e95708a9
tnaswin <tnaswin007@gmail.com>
2020-05-10 22:08:03 +0530

    Create an adoption app and define models

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aswin/Documents/Aswin/Development/learn-django/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
